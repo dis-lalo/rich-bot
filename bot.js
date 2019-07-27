19eb520b7a342d5a93cee0e842a1d1e25ebb6642
dis-lalo <53363969+dis-lalo@users.noreply.github.com>
2019-07-26 23:47:29 -0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'NjA0MzkwNDM4NTMwNTgwNDgw.XTtfMQ.znWImRDhB5xgaCEM6sYZ9to4too';
+
+bot.on('ready',() =>{
+    console.log('The bot is Online!');
+})
+
+bot.on('message', message=>{
+    if (message.content === "i want money"){
+        message.reply('Fuck You');
+
+}
+})
+
+bot.on('message', message=>{
+    if(message.content === "No fuck you"){
+       message.reply('ok have some money $');
+}
+})
+
+bot.on('message', message=>{
+    if(message.content === "info"){
+        message.reply('This bot was created by Superlalo! Version 1.0.0');
+}
+})
+bot.on('message', message=>{
+    if(message.content === "$"){
+       message.reply('https://www.timothytiah.com/wp-content/uploads/2014/06/money.jpg');
+}
+})
+bot.login(token)
